f262051241cd986deeefa4f69df507df6bcc384c
Ioannis Apanozidis <ioan0673@stud.kea.dk>
2021-02-14 05:10:20 +0100

    JavaScript ready

--- javascript/script.js
+++ javascript/script.js
@@ -9,3 +9,23 @@ function menuToggle() {
     menuItems.style.maxHeight = "0px";
   }
 }
+
+const url = "https://kea-alt-del.dk/t7/api/brands";
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => handleProductList(data));
+
+function handleProductList(data) {
+  data.forEach(printBrand);
+}
+
+function printBrand(brand) {
+  console.log(brand);
+  const template = document.querySelector("template").content;
+  const copy = template.cloneNode(true);
+  copy.querySelector("a").textContent = brand.brandname;
+  copy.querySelector("a").href = `productlist.html?brand=${brand.brandname}`;
+  const parent = document.querySelector("#brandlist");
+  parent.appendChild(copy);
+}
